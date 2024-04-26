6b2a684940df8fbc0eb8de20fd2f4fa59ef79826
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2024-04-26 13:05:55 -0500

    Update icons in Header component and fix typo in color prop

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon from 'react-native-vector-icons/Feather';
 import { styles } from './Header.styles';
 
 const Header: React.FC = () => {
@@ -9,10 +9,10 @@ const Header: React.FC = () => {
             <Text style={styles.headerTitle}>Notes</Text>
             <View style={styles.headerIcons}>
                 <TouchableOpacity style={styles.iconStyle}>
-                    <Icon name="menu" size={24} color="#fff" />
+                    <Icon name="search" size={24} color="#fff" />
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.iconStyle}>
-                    <Icon name="refresh" size={24} color="#sfff" />
+                    <Icon name="alert-circle" size={24} color="#fff" />
                 </TouchableOpacity>
             </View>
         </View>
--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FlatList } from 'react-native';
 import Note from '../Note/Note';
+import { FlatList } from 'react-native';
 import { styles } from './NoteList.styles';
 
 const noteColors = ['#FFADAD', '#FFD6A5', '#FDFFB6', '#CAFFBF', '#9BF6FF', '#A0C4FF', '#BDB2FF', '#FFC6FF'];
